9b0066b93c8d0ab9e6f2458d354cd9b55d05f29d
Grant Rettke <gcr@wisdomandwonder.com>
2017-11-14 01:30:41 -0600

    for every package use use-package

--- pie.el
+++ pie.el
@@ -5,3 +5,10 @@
 (add-to-list 'package-archives
              '("melpa-stable" . "https://stable.melpa.org/packages/"))
 (package-initialize)
+
+;; for every package use use-package
+(unless (package-installed-p 'use-package)
+  (package-refresh-contents)
+  (package-install 'use-package))
+(require 'use-package)
+(setq use-package-always-ensure t)

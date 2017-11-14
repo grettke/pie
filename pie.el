b7c497f385a803a3ca8234208a2a4cc53d25cafd
Grant Rettke <gcr@wisdomandwonder.com>
2017-11-14 01:26:11 -0600

    only stable packages

--- pie.el
+++ pie.el
@@ -1,1 +1,7 @@
 ;;; packages
+
+;; only stable packages
+(require 'package)
+(add-to-list 'package-archives
+             '("melpa-stable" . "https://stable.melpa.org/packages/"))
+(package-initialize)

da0c118d38b500a1b6864fc680b026367f545a3c
Grant Rettke <gcr@wisdomandwonder.com>
2017-11-14 01:54:14 -0600

    always start to an empty buffer

--- pie.el
+++ pie.el
@@ -31,3 +31,21 @@
 
 ;; default major mode Markdown
 (setq initial-major-mode 'markdown-mode)
+
+;; always start to an empty buffer
+(defun xah-new-empty-buffer ()
+  "Create a new empty buffer.
+New buffer will be named “untitled” or “untitled<2>”, “untitled<3>”, etc.
+
+It returns the buffer (for elisp programing).
+
+URL `http://ergoemacs.org/emacs/emacs_new_empty_buffer.html'
+Version 2017-11-01"
+  (interactive)
+  (let (($buf (generate-new-buffer "Untitled.md")))
+    (switch-to-buffer $buf)
+    (funcall initial-major-mode)
+    (setq buffer-offer-save t)
+    $buf
+    ))
+(setq initial-buffer-choice 'xah-new-empty-buffer)
